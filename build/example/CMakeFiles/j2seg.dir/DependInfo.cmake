e54b0adad7db8bdd4a8ad68a6684050d6f5429b1
411-3 <2250611484@qq.com>
2017-02-20 11:54:02 +0800

    slic_image_lib

---
+++ build/example/CMakeFiles/j2seg.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xduser/LiHuan/SLICO/example/j2seg.cpp" "/home/xduser/LiHuan/SLICO/build/example/CMakeFiles/j2seg.dir/j2seg.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/xduser/LiHuan/SLICO/build/src/CMakeFiles/SLIC.dir/DependInfo.cmake"
+  )
---
+++ build/src/CMakeFiles/opencv_lbp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/opencv_lbp.dir/opencv_lbp.cpp.o"
+  "libopencv_lbp.pdb"
+  "libopencv_lbp.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/opencv_lbp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
